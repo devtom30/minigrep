53b1606580f1db8a89c6c6b4c6a0741a910014fc
devtom30 <devtom30@gmail.com>
2024-05-01 12:10:17 +0200

    using iter

--- src/lib.rs
+++ src/lib.rs
@@ -51,13 +51,10 @@ pub fn search<'a>(
     query: &str,
     contents: &'a str,
 ) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
